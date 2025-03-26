dc7cfe25384d399e709e69296ce4a058c2aed905
Brian M <brian.mak.sy@gmail.com>
2025-03-26 16:51:29 +0100

    get fragments without transliteration

--- utils/connection.py
+++ utils/connection.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 mongodb_uri = os.getenv('MONGODB_URI', '')
-mongodb_username = os.getenv('MONGODB_USERNAME', '')
-mongodb_password = os.getenv('MONGODB_PASSWORD', '')
-client = MongoClient(mongodb_uri, username=mongodb_username, password=mongodb_password)
+client = MongoClient(mongodb_uri)
 db = client['ebl']
 fragments = db['fragments']
+
+# find fragments with no transliteration
+empty_fragments = fragments.find({"text.lines.0": {"$exists": False}})
