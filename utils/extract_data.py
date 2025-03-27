c7bb07a567b7c3477c838c3071c143dc9c0c49cd
Brian M <brian.mak.sy@gmail.com>
2025-03-27 18:24:44 +0100

    wip: try to write 'signs' property to 'fragments' coll for one tablet

--- utils/extract_data.py
+++ utils/extract_data.py
@@ -1,5 +1,8 @@
+# standard imports
+import json
 # package imports
-# file imports
+from pymongo import ReturnDocument
+# local imports
 from connection import get_connection
 ########################################################
 def get_annotated_fragments_ids(fragments_collection):
@@ -9,6 +12,9 @@ def get_annotated_fragments_ids(fragments_collection):
         annotated_fragments_ids.add(fragment.get('fragmentNumber'))
     return annotated_fragments_ids
 
+def read_json_file(file_name):
+    with open(file_name) as f:
+        return json.load(f)
 
 if __name__ == '__main__':
     client = get_connection()
@@ -17,9 +23,19 @@ if __name__ == '__main__':
     annotations = db['annotations']
 
     # exclude fragments with no transliteration and fragments already annotated
-    non_empty_fragments_query = {"text.lines.0": {"$exists": True}}
-    annotated_fragments_ids = get_annotated_fragments_ids(annotations)
-    filter_query = {"_id": {"$nin": list(annotated_fragments_ids)}}
-    fragments_to_match = fragments.find({**non_empty_fragments_query, **filter_query})
+#    non_empty_fragments_query = {"text.lines.0": {"$exists": True}}
+#    annotated_fragments_ids = get_annotated_fragments_ids(annotations)
+#    filter_query = {"_id": {"$nin": list(annotated_fragments_ids)}}
+#    fragments_to_match = fragments.find({**non_empty_fragments_query, **filter_query})
+
+    # update "signs" for one fragment (test) (to do at the end in any case)
+    file_name = "eBL_OCRed_Signs.json"
+    ocr_signs_array = read_json_file(file_name)
+    ocred_signs_for_one_file = ocr_signs_array[0].get("ocredSigns")
+    fragment_name = ocr_signs_array[0].get("filename").split(".jpg")[0]
+
+    doc = fragments.find_one_and_update({'_id': fragment_name},{'$set': {'signs': ocred_signs_for_one_file}},return_document=ReturnDocument.AFTER)
+
     breakpoint()
+
 
