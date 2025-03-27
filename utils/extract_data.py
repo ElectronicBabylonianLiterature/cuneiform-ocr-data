932cbc7af4717b24d070087a417a0b71d79cdb90
Brian M <brian.mak.sy@gmail.com>
2025-03-27 20:07:01 +0100

    wip: generate snippets and metadata to be saved locally according to sign number

--- utils/extract_data.py
+++ utils/extract_data.py
@@ -1,7 +1,11 @@
 # standard imports
 import json
+import base64
+from io import BytesIO
 # package imports
 from pymongo import ReturnDocument
+from PIL import Image
+import gridfs
 # local imports
 from connection import get_connection
 ########################################################
@@ -15,6 +19,22 @@ def get_annotated_fragments_ids(fragments_collection):
 def read_json_file(file_name):
     with open(file_name) as f:
         return json.load(f)
+
+def generate_ocr_signs_to_coordinates_tuples(ocr_signs_entry):
+    ocr_signs_string = ocr_signs_entry["ocredSigns"]
+    ocr_signs_array = ocr_signs_string.split()
+    return list(zip(ocr_signs_array, ocr_signs_entry["ocredSignsCoordinates"]))
+
+def decode_binary_to_image(img_binary):
+    return Image.open(BytesIO(img_binary))
+
+def crop_image(image, coordinates):
+    return image.crop(coordinates)
+
+def encode_image_to_base64(image):
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    return base64.b64encode(buffered.getvalue()).decode('utf-8')
 
 if __name__ == '__main__':
     client = get_connection()
@@ -34,8 +54,32 @@ if __name__ == '__main__':
     ocred_signs_for_one_file = ocr_signs_array[0].get("ocredSigns")
     fragment_name = ocr_signs_array[0].get("filename").split(".jpg")[0]
 
-    doc = fragments.find_one_and_update({'_id': fragment_name},{'$set': {'signs': ocred_signs_for_one_file}},return_document=ReturnDocument.AFTER)
+    # doc = fragments.find_one_and_update({'_id': fragment_name},{'$set': {'signs': ocred_signs_for_one_file}},return_document=ReturnDocument.AFTER)
 
-    breakpoint()
 
+    # extract photos
+    photos = gridfs.GridFS(db, collection="photos")
+    for ocr_signs_fragment in ocr_signs_array[:1]:
+        filename = ocr_signs_fragment.get("filename")
+        # find photo base64string
+        photo_file = photos.find_one({"filename": filename})
+
+        file_data = photo_file.read()
+
+        image = decode_binary_to_image(file_data)
+
+        sign_coord_tuples = generate_ocr_signs_to_coordinates_tuples(ocr_signs_fragment)
+        for sign_coord_tuple in sign_coord_tuples:
+            # TODO add tqdm progress bar
+            sign_name, coordinates = sign_coord_tuple
+            crop = crop_image(image, coordinates)
+            sign_base_64 = encode_image_to_base64(crop)
+
+            # to write: sign name, sign coord, source image
+            metadata = {
+                "source_image": file_name,
+                "image_coordinates": coordinates
+            }
+            breakpoint()
+            crop.show()
 
