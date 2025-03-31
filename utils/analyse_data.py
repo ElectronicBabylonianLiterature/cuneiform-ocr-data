5f8aa6953f18a8dc1214cee59dc68f594b26c81f
Brian M <brian.mak.sy@gmail.com>
2025-03-31 15:35:33 +0200

    analyse 'naive' sign set

---
+++ utils/analyse_data.py
@@ -0,0 +1,22 @@
+import json
+from collections import defaultdict
+
+def generate_statistics(metadata):
+    stats = defaultdict(int)
+    summary = dict()
+    for d in metadata:
+        sign = d["sign"]
+        stats[sign] += 1
+    summary["num_of_diff_signs"] = len(stats.keys())
+    summary["sign_total"] = sum(stats.values())
+    return stats, summary
+
+def load_json_metadata():
+    with open("naive_data/crops_metadata.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        return data
+
+if __name__ == '__main__':
+    metadata = load_json_metadata()
+    stats, summary = generate_statistics(metadata )
+    breakpoint()
