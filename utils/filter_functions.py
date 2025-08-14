f121b56299ddddb96cba4835b3e2e8b89e8d2e01
Brian M <brian.mak.sy@gmail.com>
2025-08-14 12:55:56 +0200

    wip: remove broken away signs

--- utils/filter_functions.py
+++ utils/filter_functions.py
@@ -1,4 +1,7 @@
-from cuneiform_ocr_data.sign_mappings.mappings import build_sign_dict, build_sign_to_abz_dict
+import json
+import pytest 
+
+from cuneiform_ocr_data.sign_mappings.mappings import build_sign_to_abz_dict
 from utils.connection import get_connection
 
 ########################################################
@@ -22,15 +25,19 @@ def generate_value_to_abz_dict(signs_coll, sign_to_abz_dict):
                 "message": str(e),
                 "traceback": traceback.format_exc()
             }
-            print(error_info)
+            # print(error_info)
     return value_to_abz_dict
 
-def remove_completions_from_transliteration():
-    # check with { _id: "1879,0708.124" }
+
+def remove_completions_from_transliteration(fragment_signs, fragment_text_lines):
+    fragment_array = fragment_signs.split('\n')
+    # for line in fragment_array:
+
     return 
 
-def test_remove_completions_from_transliteration():
-    fragment_object = ''' 
+@pytest.fixture 
+def fragment_signs():
+    return ''' 
       X ABZ401 X
       ABZ401 ABZ461 ABZ457 ABZ73
       ABZ401 ABZ461 ABZ457 ABZ73 ABZ597 ABZ470 X X X X X X X X X
@@ -46,7 +53,10 @@ def test_remove_completions_from_transliteration():
       ABZ296 ABZ401 ABZ296 ABZ314 ABZ457 ABZ134 ABZ61 ABZ480 ABZ279
       ABZ296 ABZ401 ABZ296 ABZ314
     '''
-    target_object = ''' 
+
+@pytest.fixture
+def target_signs():
+    return '''
       X ABZ401 X
       ABZ461 ABZ457 
       ABZ461 ABZ457 ABZ73 ABZ597 ABZ470 X X X X X X X X
@@ -62,14 +72,56 @@ def test_remove_completions_from_transliteration():
       ABZ401 ABZ296 ABZ314 ABZ457 ABZ134 ABZ61 ABZ480 ABZ279
       ABZ401 ABZ296 
     ''' 
-    assert True
+
+def test_remove_completions_from_transliteration(fragment_signs, target_signs):
+    """ Check with { _id: "1879,0708.124" } """
+    trimmed_transliteration = remove_completions_from_transliteration(fragment_signs)
+
+
+    assert trimmed_transliteration == target_signs
   
+def write_fragment_text_lines(fragment_text_lines):
+    with open("utils/fragment_text_object.json", "w", encoding="utf-8") as f:
+        json.dump(fragment_text_lines,f, indent=4)
 
+def read_fragment_text_object():
+    with open("utils/fragment_text_object.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        return data
 
+def fragment_text_from_db():
+    fragments = db['fragments']
+    fragment_number = "1879,0708.124" 
+    fragment_object = fragments.find_one({"_id": fragment_number})
+    fragment_text_lines = fragment_object["text"]["lines"]
+    return fragment_text_lines 
+    
 if __name__ == '__main__':
     client = get_connection()
     db = client['ebl']
     signs = db['signs']
     sign_to_abz_dict = build_sign_to_abz_dict()
     value_to_abz_dict = generate_value_to_abz_dict(signs, sign_to_abz_dict)
+
+    # get fragment text and transliteration
+    fragment_text_lines = read_fragment_text_object()
+    tablet_indices = [[] for _ in range(len(fragment_text_lines))]
+    for line_idx, line in enumerate(fragment_text_lines):
+        content = [unit["value"] for unit in line["content"] if '...' not in unit["value"]]
+        content = [part for item in content for part in item.split('-')]
+        unit_after_break = 0
+        unit_before_end_break = len(content)-1
+        for i, unit_value in enumerate(content):
+            if ']' in unit_value and i + 1 < len(content):
+                unit_after_break = i + 1
+                continue
+        for i in range(unit_after_break, len(content)):
+            if '[' in unit_value and i - 1 >= 0:
+                unit_before_end_break = i - 1
+            
+        tablet_indices[line_idx].append((unit_after_break, unit_before_end_break))
+        breakpoint()
     breakpoint()
+
+
+        # if "]" in
