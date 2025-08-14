c80d9794b37bd26d65a3e2a1bb44c861425ff9bc
Brian M <brian.mak.sy@gmail.com>
2025-08-14 13:56:54 +0200

    wip: remove broken away transliteration

--- utils/filter_functions.py
+++ utils/filter_functions.py
@@ -1,5 +1,6 @@
 import json
 import pytest 
+import re
 
 from cuneiform_ocr_data.sign_mappings.mappings import build_sign_to_abz_dict
 from utils.connection import get_connection
@@ -29,48 +30,56 @@ def generate_value_to_abz_dict(signs_coll, sign_to_abz_dict):
     return value_to_abz_dict
 
 
-def remove_completions_from_transliteration(fragment_signs, fragment_text_lines):
-    fragment_array = fragment_signs.split('\n')
+def remove_completions_from_transliteration(FRAGMENT_SIGNS_STRING, fragment_text_lines):
+    fragment_array = [line.strip() for line in FRAGMENT_SIGNS_STRING.splitlines() if line.strip()]
+    breakpoint()
+    indices_to_retain = get_fragment_indices_to_retain(fragment_text_lines)
+
+
     # for line in fragment_array:
 
     return 
 
 @pytest.fixture 
 def fragment_signs():
-    return ''' 
-      X ABZ401 X
-      ABZ401 ABZ461 ABZ457 ABZ73
-      ABZ401 ABZ461 ABZ457 ABZ73 ABZ597 ABZ470 X X X X X X X X X
-      ABZ480 ABZ99 ABZ87 ABZ15 ABZ55 ABZ376 ABZ80 ABZ85
-      ABZ61 ABZ231 ABZ1 ABZ461 ABZ61 ABZ483 ABZ115 ABZ296 ABZ401 ABZ296 ABZ314 ABZ342 ABZ13 ABZ360 ABZ114 ABZ457 ABZ78 ABZ393 ABZ231 ABZ537 ABZ69
-      ABZ69 ABZ1 ABZ354 ABZ112 ABZ401 ABZ461 ABZ457 ABZ73 ABZ597 ABZ470 ABZ480 ABZ449 ABZ106 ABZ401 ABZ296 ABZ401 ABZ296 ABZ314 ABZ342 ABZ381 ABZ206 ABZ13 ABZ323 ABZ533 ABZ68 ABZ533 ABZ480 ABZ87 ABZ111 ABZ533 ABZ231
-      ABZ69 ABZ411 ABZ411 ABZ318 ABZ61 ABZ231 ABZ1 ABZ461 ABZ570 ABZ296 ABZ314 ABZ342 ABZ480 ABZ384 ABZ75 ABZ68 ABZ381 ABZ84 ABZ381 ABZ393 ABZ61 ABZ480 ABZ366 ABZ60
-      ABZ61 ABZ231 ABZ461 ABZ570 150 ABZ449 ABZ296 ABZ536 ABZ533 ABZ322 ABZ533 ABZ230 ABZ533
-      ABZ411 ABZ401 ABZ461 ABZ457 ABZ73 ABZ597 ABZ470 ABZ480 ABZ449 ABZ106 ABZ401 ABZ296 ABZ314 ABZ342 ABZ208 ABZ401 ABZ597 150 ABZ296 ABZ69
-      ABZ381 ABZ354 ABZ7 ABZ366 ABZ10
-      ABZ13 ABZ360 ABZ114 ABZ80 ABZ172 ABZ1 ABZ38 ABZ331e+152i ABZ398
-      ABZ401 ABZ597 ABZ470 ABZ295 ABZ296 ABZ209 ABZ393 ABZ61 ABZ60 ABZ1 ABZ396 ABZ597 ABZ126 ABZ10
-      ABZ296 ABZ401 ABZ296 ABZ314 ABZ457 ABZ134 ABZ61 ABZ480 ABZ279
-      ABZ296 ABZ401 ABZ296 ABZ314
-    '''
+    return FRAGMENT_SIGNS_STRING 
+
+FRAGMENT_SIGNS_STRING = ''' 
+X ABZ401 X
+ABZ401 ABZ461 ABZ457 ABZ73
+ABZ401 ABZ461 ABZ457 ABZ73 ABZ597 ABZ470 X X X X X X X X X
+ABZ480 ABZ99 ABZ87 ABZ15 ABZ55 ABZ376 ABZ80 ABZ85
+ABZ61 ABZ231 ABZ1 ABZ461 ABZ61 ABZ483 ABZ115 ABZ296 ABZ401 ABZ296 ABZ314 ABZ342 ABZ13 ABZ360 ABZ114 ABZ457 ABZ78 ABZ393 ABZ231 ABZ537 ABZ69
+ABZ69 ABZ1 ABZ354 ABZ112 ABZ401 ABZ461 ABZ457 ABZ73 ABZ597 ABZ470 ABZ480 ABZ449 ABZ106 ABZ401 ABZ296 ABZ401 ABZ296 ABZ314 ABZ342 ABZ381 ABZ206 ABZ13 ABZ323 ABZ533 ABZ68 ABZ533 ABZ480 ABZ87 ABZ111 ABZ533 ABZ231
+ABZ69 ABZ411 ABZ411 ABZ318 ABZ61 ABZ231 ABZ1 ABZ461 ABZ570 ABZ296 ABZ314 ABZ342 ABZ480 ABZ384 ABZ75 ABZ68 ABZ381 ABZ84 ABZ381 ABZ393 ABZ61 ABZ480 ABZ366 ABZ60
+ABZ61 ABZ231 ABZ461 ABZ570 150 ABZ449 ABZ296 ABZ536 ABZ533 ABZ322 ABZ533 ABZ230 ABZ533
+ABZ411 ABZ401 ABZ461 ABZ457 ABZ73 ABZ597 ABZ470 ABZ480 ABZ449 ABZ106 ABZ401 ABZ296 ABZ314 ABZ342 ABZ208 ABZ401 ABZ597 150 ABZ296 ABZ69
+ABZ381 ABZ354 ABZ7 ABZ366 ABZ10
+ABZ13 ABZ360 ABZ114 ABZ80 ABZ172 ABZ1 ABZ38 ABZ331e+152i ABZ398
+ABZ401 ABZ597 ABZ470 ABZ295 ABZ296 ABZ209 ABZ393 ABZ61 ABZ60 ABZ1 ABZ396 ABZ597 ABZ126 ABZ10
+ABZ296 ABZ401 ABZ296 ABZ314 ABZ457 ABZ134 ABZ61 ABZ480 ABZ279
+ABZ296 ABZ401 ABZ296 ABZ314
+'''
 
 @pytest.fixture
 def target_signs():
-    return '''
-      X ABZ401 X
-      ABZ461 ABZ457 
-      ABZ461 ABZ457 ABZ73 ABZ597 ABZ470 X X X X X X X X
-      ABZ55 ABZ376 ABZ80 ABZ85
-      ABZ115 ABZ296 ABZ401 ABZ296 ABZ314 ABZ342 ABZ13 ABZ360 ABZ114 ABZ457 ABZ78 ABZ393 ABZ231 ABZ537 ABZ69
-      ABZ296 ABZ401 ABZ296 ABZ314 ABZ342 ABZ381 ABZ206 ABZ13 ABZ323 ABZ533 ABZ68 ABZ533 ABZ480 ABZ87 ABZ111 ABZ533 ABZ231
-      ABZ314 ABZ342 ABZ480 ABZ384 ABZ75 ABZ68 ABZ381 ABZ84 ABZ381 ABZ393 ABZ61 ABZ480 ABZ366 ABZ60
-      150 ABZ449 ABZ296 ABZ536 ABZ533 ABZ322 ABZ533 ABZ230 ABZ533
-      ABZ457 ABZ73 ABZ597 ABZ470 ABZ480 ABZ449 ABZ106 ABZ401 ABZ296 ABZ314 ABZ342 ABZ208 ABZ401 ABZ597 150 ABZ296 ABZ69
-      ABZ381 ABZ354 ABZ7 ABZ366 ABZ10
-      ABZ114 ABZ80 ABZ172 ABZ1 ABZ38 ABZ331e+152i ABZ398
-      ABZ597 ABZ470 ABZ295 ABZ296 ABZ209 ABZ393 ABZ61 ABZ60 ABZ1 ABZ396 ABZ597 ABZ126 ABZ10
-      ABZ401 ABZ296 ABZ314 ABZ457 ABZ134 ABZ61 ABZ480 ABZ279
-      ABZ401 ABZ296 
+    return TARGET_SIGNS_STRING
+
+TARGET_SIGNS_STRING = '''
+    X ABZ401 X
+    ABZ461 ABZ457 
+    ABZ461 ABZ457 ABZ73 ABZ597 ABZ470 X X X X X X X X
+    ABZ55 ABZ376 ABZ80 ABZ85
+    ABZ115 ABZ296 ABZ401 ABZ296 ABZ314 ABZ342 ABZ13 ABZ360 ABZ114 ABZ457 ABZ78 ABZ393 ABZ231 ABZ537 ABZ69
+    ABZ296 ABZ401 ABZ296 ABZ314 ABZ342 ABZ381 ABZ206 ABZ13 ABZ323 ABZ533 ABZ68 ABZ533 ABZ480 ABZ87 ABZ111 ABZ533 ABZ231
+    ABZ314 ABZ342 ABZ480 ABZ384 ABZ75 ABZ68 ABZ381 ABZ84 ABZ381 ABZ393 ABZ61 ABZ480 ABZ366 ABZ60
+    150 ABZ449 ABZ296 ABZ536 ABZ533 ABZ322 ABZ533 ABZ230 ABZ533
+    ABZ457 ABZ73 ABZ597 ABZ470 ABZ480 ABZ449 ABZ106 ABZ401 ABZ296 ABZ314 ABZ342 ABZ208 ABZ401 ABZ597 150 ABZ296 ABZ69
+    ABZ381 ABZ354 ABZ7 ABZ366 ABZ10
+    ABZ114 ABZ80 ABZ172 ABZ1 ABZ38 ABZ331e+152i ABZ398
+    ABZ597 ABZ470 ABZ295 ABZ296 ABZ209 ABZ393 ABZ61 ABZ60 ABZ1 ABZ396 ABZ597 ABZ126 ABZ10
+    ABZ401 ABZ296 ABZ314 ABZ457 ABZ134 ABZ61 ABZ480 ABZ279
+    ABZ401 ABZ296 
     ''' 
 
 def test_remove_completions_from_transliteration(fragment_signs, target_signs):
@@ -96,6 +105,45 @@ def fragment_text_from_db():
     fragment_text_lines = fragment_object["text"]["lines"]
     return fragment_text_lines 
     
+def flatten_line_content(line_content):
+    """Flatten content of each line in fragment_object["text"]["lines"] """
+    content_split_on_dash = [part for item in line_content for part in item.split('-')]
+    # content = [part for item in content_split_on_dash for part in item.split('.')]
+    content_split_on_dot = [ part for el in content_split_on_dash for part in re.split(r'(?<!\.)\.(?!\.)', el) ]
+    content_no_indent = [el for el in content_split_on_dot if el != '($___$)']
+    flat_list = [
+        part
+            for el in content_no_indent
+            for part in ([el[el.find('{')+1 : el.find('}')], el[el.find('}')+1:]] if '{' in el and '}' in el else [el])
+        ]
+    return flat_list
+
+def get_element_indices_not_in_square_brackets(content):
+    result_indices = []
+    inside_brackets = False
+
+    for i, el in enumerate(content):
+        if el.startswith('[') and el.endswith(']'):
+            continue
+        if '[' in el:
+            inside_brackets = True
+        if not inside_brackets:
+            result_indices.append(i)
+        if ']' in el:
+            inside_brackets = False
+
+    # all_indices = set(range(len(content)))
+    # result_indices = [i for i in all_indices if i not in indices_in_brackets]
+    return result_indices
+def get_fragment_indices_to_retain(fragment_text_lines):
+    tablet_indices = []
+    for line in fragment_text_lines:
+        line_content = [unit["value"] for unit in line["content"] ]
+        content = flatten_line_content(line_content)
+        content_indices = get_element_indices_not_in_square_brackets(content)
+  
+        tablet_indices.append(content_indices)
+    return tablet_indices
 if __name__ == '__main__':
     client = get_connection()
     db = client['ebl']
@@ -103,25 +151,9 @@ if __name__ == '__main__':
     sign_to_abz_dict = build_sign_to_abz_dict()
     value_to_abz_dict = generate_value_to_abz_dict(signs, sign_to_abz_dict)
 
-    # get fragment text and transliteration
+    # get fragment text and filter transliteration
     fragment_text_lines = read_fragment_text_object()
-    tablet_indices = [[] for _ in range(len(fragment_text_lines))]
-    for line_idx, line in enumerate(fragment_text_lines):
-        content = [unit["value"] for unit in line["content"] if '...' not in unit["value"]]
-        content = [part for item in content for part in item.split('-')]
-        unit_after_break = 0
-        unit_before_end_break = len(content)-1
-        for i, unit_value in enumerate(content):
-            if ']' in unit_value and i + 1 < len(content):
-                unit_after_break = i + 1
-                continue
-        for i in range(unit_after_break, len(content)):
-            if '[' in unit_value and i - 1 >= 0:
-                unit_before_end_break = i - 1
-            
-        tablet_indices[line_idx].append((unit_after_break, unit_before_end_break))
-        breakpoint()
-    breakpoint()
+    indices_to_retain = get_fragment_indices_to_retain(fragment_text_lines)
 
-
+    filtered_transliteration = remove_completions_from_transliteration(FRAGMENT_SIGNS_STRING, indices_to_retain) 
         # if "]" in
