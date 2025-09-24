eb944a3108c5b437a287e53ed4d447f53f93ed38
Brian M <brian.mak.sy@gmail.com>
2025-09-24 22:58:46 +0200

    conversion of absolute to relative geom succeed, tests pass

--- crop_ocr_signs/verify_signs/match_coordinates/generate_annotations_json.py
+++ crop_ocr_signs/verify_signs/match_coordinates/generate_annotations_json.py
@@ -7,6 +7,8 @@ from pydantic import BaseModel
 from tqdm import tqdm
 
 # local imports
+from crop_ocr_signs.connection import get_connection
+from crop_ocr_signs.convenience_methods.show_crop_in_image import get_image_from_file_name
 from crop_ocr_signs.convenience_methods.update_with_disambiguated_reading import make_disambiguated_dict
 from crop_ocr_signs.extract_data import read_json_file, transform_signs_array_to_signs_dict
 from crop_ocr_signs.verify_signs.get_partial_order_signs import get_crops_groupped_by_fragments, get_ocred_signs 
@@ -25,7 +27,37 @@ class AnnotationObject(BaseModel):
     fragmentNumber: str 
     annotations: List[Annotation]
 
-def get_annotations_for_signs(cropped_ocred_signs_and_index_array, ocr_signs_dict, fragment_number):
+def xyxy_to_xywh(coords):
+    """Convert (x1, y1, x2, y2) to (x, y, width, height)."""
+    x1, y1, x2, y2 = coords
+    return x1, y1, x2-x1, y2-y1
+
+def convert_abs_coordinates_to_relative_geometry(abs_coordinates, image_width, image_height):
+    """
+    Get absolute coordinates from OCR, e.g. 
+    [921, 472, 1163, 565] for SAG in K.2210
+    and convert to the relative geometry found in the annotations DB collection:
+    {
+        "x": 51.4807302231237,
+        "y": 19.2471406891957,
+        "width": 13.5496957403651,
+        "height": 3.79020616648776
+    }
+    """
+    abs_xywh = xyxy_to_xywh(abs_coordinates) 
+    absolute_x, absolute_y, absolute_width, absolute_height = abs_xywh 
+    relative_x = absolute_x * 100 / image_width
+    relative_y = absolute_y * 100 / image_height
+    relative_width = absolute_width * 100 / image_width
+    relative_height = absolute_height * 100 / image_height
+    return {
+        "x":relative_x,
+        "y":relative_y,
+        "width":relative_width,
+        "height":relative_height
+        }
+
+def get_annotations_for_signs(db, cropped_ocred_signs_and_index_array, ocr_signs_dict, fragment_number):
     """
     For crops of a tablet, e.g. 
         [('ABZ214', '1'), ('ABZ99', '2'), ('ABZ214', '3'), ('ABZ15', '4'), ('ABZ128', '5'), ('ABZ86', '8'), ('ABZ212', '9'), ('ABZ84', '11'), ('ABZ61', '12')]
@@ -34,13 +66,19 @@ def get_annotations_for_signs(cropped_ocred_signs_and_index_array, ocr_signs_dic
 
     For spotchecking, run `convenience_methods`, show_crop_in_image.py 
     """
-    ocr_coordinates_list = ocr_signs_dict[f"{fragment_number}.jpg"]["ocredSignsCoordinates"]
+    photo_file_name = f"{fragment_number}.jpg"
+    ocr_coordinates_list = ocr_signs_dict[photo_file_name]["ocredSignsCoordinates"]
     abz_sign_dict = make_disambiguated_dict()
     
     annotations = [] 
     for sign, idx in cropped_ocred_signs_and_index_array:
         annotation = defaultdict()
-        annotation["geometry"] = ocr_coordinates_list[int(idx)]
+
+        abs_coordinates = ocr_coordinates_list[int(idx)]
+        image = get_image_from_file_name(photo_file_name, db)
+        relative_geom = convert_abs_coordinates_to_relative_geometry(abs_coordinates, *image.size)
+
+        annotation["geometry"] = relative_geom 
         annotation["data"] = {
             "id": f"{sign}_{idx}",
             "type": "OCR",
@@ -53,6 +91,8 @@ def get_annotations_for_signs(cropped_ocred_signs_and_index_array, ocr_signs_dic
 
 
 if __name__ == '__main__':
+    client = get_connection()
+    db = client['ebl']
     verified_crops_folder = 'signs_40k_ocr_no_partial_order'
     crops_groupped_by_fragment = get_crops_groupped_by_fragments(verified_crops_folder)
     ocr_signs_array = read_json_file()
@@ -62,6 +102,6 @@ if __name__ == '__main__':
         crops_of_fragment = crops_groupped_by_fragment[fragment_number]
         # get ocredSigns and match with coordinates
         cropped_ocred_signs_and_index_array = get_ocred_signs(fragment_number, ocr_signs_dict, crops_of_fragment)
-        get_annotations_for_signs(cropped_ocred_signs_and_index_array,ocr_signs_dict, fragment_number)
+        get_annotations_for_signs(db, cropped_ocred_signs_and_index_array,ocr_signs_dict, fragment_number)
 
         
--- crop_ocr_signs/verify_signs/match_coordinates/test_coordinates_correspond_to_sign.py
+++ crop_ocr_signs/verify_signs/match_coordinates/test_coordinates_correspond_to_sign.py
@@ -5,35 +5,51 @@ import pytest
 from crop_ocr_signs.connection import get_connection
 from crop_ocr_signs.convenience_methods.show_crop_in_image import get_image_from_file_name
 from crop_ocr_signs.extract_data import crop_image
+################################################################
 
 def xywh_to_xyxy(coords: Tuple[float, float, float, float]) -> Tuple[float, float, float, float]:
     """Convert (x, y, width, height) to (x1, y1, x2, y2)."""
     x, y, w, h = coords
     return x, y, x + w, y + h
 
-def test_check_ocr_coordinates_match_sign():
-    """
-    fragment_number
-    """
-    photo_file_name = "K.2210.jpg"
-    geometry = {
+def from_relative_coordinates(
+        relative_x,
+        relative_y,
+        relative_width,
+        relative_height,
+        image_width,
+        image_height,
+    ):
+    absolute_x = int(round(relative_x / 100 * image_width))
+    absolute_y = int(round(relative_y / 100 * image_height))
+    absolute_width = int(round(relative_width / 100 * image_width))
+    absolute_height = int(round(relative_height / 100 * image_height))
+
+    return absolute_x, absolute_y, absolute_width, absolute_height
+
+@pytest.mark.parametrize("photo_file_name, target_sign, geometry", [
+    ("K.2210.jpg", "SAG", {
         "x": 51.4807302231237,
         "y": 19.2471406891957,
         "width": 13.5496957403651,
         "height": 3.79020616648776
-    }
-    target_sign = "SAG"
-    geometry_tuple = (geometry["x"], geometry["y"], geometry["width"], geometry["height"])
-    coordinates_xyxy = xywh_to_xyxy(geometry_tuple)
-
-
+    }),
+    ('K.13973.jpg', 'BI', {
+        "x": 20.486651649441068, 
+        "y": 21.25577256559395, 
+        "width": 12.258344883370038,
+        "height": 6.828714212345857
+    }) 
+])
+def test_check_ocr_coordinates_match_sign(photo_file_name, target_sign, geometry):
     client = get_connection()
     db = client['ebl']
     image = get_image_from_file_name(photo_file_name, db)
-    if image:
-        crop = crop_image(image, coordinates_xyxy)
-        crop.show()
-        pytest.fail("Forcing failure here")
 
-# if __name__ == '__main__':
-    # check_ocr_coordinates_match_sign() 
+    geometry_tuple = (geometry["x"], geometry["y"], geometry["width"], geometry["height"])
+    abs_geom = from_relative_coordinates(*geometry_tuple, *image.size) 
+    abs_coords = xywh_to_xyxy(abs_geom)
+    if image:
+        crop = crop_image(image, abs_coords)
+        crop.show()
+        assert True
