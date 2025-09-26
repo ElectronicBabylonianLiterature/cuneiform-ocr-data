1b30844f3b4e2bbb291037df75203f8b2374f3d0
Brian M <brian.mak.sy@gmail.com>
2025-09-26 14:04:11 +0100

    tidy up for PR

--- crop_ocr_signs/filter_functions.py
+++ crop_ocr_signs/filter_functions.py
@@ -30,7 +30,7 @@ def generate_value_to_abz_dict(signs_coll, sign_to_abz_dict):
 
 def write_fragment_text_lines(fragment_text_lines):
     with open("crop_ocr_signs/fragment_text_object.json", "w", encoding="utf-8") as f:
-        json.dump(fragment_text_lines,f, indent=4)
+        json.dump(fragment_text_lines,f, indent=2)
 
 def fragment_text_from_db():
     fragments = db['fragments']
@@ -45,8 +45,8 @@ def write_abz_with_more_than_one_value_dict():
     ocred_abz_to_sign_dict = {k:v for k,v in abz_to_signs_dict.items() if k in signs_outputted_from_ocr}
     unsorted_abz_with_more_than_one_value = {k:v for k,v in ocred_abz_to_sign_dict.items() if len(v) > 1}
     abz_with_more_than_one_value = {key: unsorted_abz_with_more_than_one_value[key] for key in sorted(unsorted_abz_with_more_than_one_value, key=natural_key)}
-    with open("abz_with_more_than_one_value.json", "w", encoding="utf-8") as f:
-        json.dump(abz_with_more_than_one_value, f, indent=4)
+    with open("crop_ocr_signs/convenience_methods/abz_with_more_than_one_value.json", "w", encoding="utf-8") as f:
+        json.dump(abz_with_more_than_one_value, f, ensure_ascii=False, indent=4)
 
 
 signs_outputted_from_ocr = ['ABZ1', 'ABZ279', 'ABZ384', 'ABZ537', 'ABZ15', 'ABZ13', 'ABZ342', 'ABZ354', 'ABZ381', 'ABZ206', 'ABZ318', 'ABZ449', 'ABZ69', 'ABZ597', 'ABZ75', 'ABZ231', 'ABZ295', 'ABZ58', 'ABZ545', 'ABZ214', 'ABZ144', 'ABZ533', 'ABZ151', 'ABZ70', 'ABZ324', 'ABZ139', 'ABZ343', 'ABZ461', 'ABZ52', 'ABZ142', 'ABZ399', 'ABZ99', 'ABZ427', 'ABZ586', 'ABZ579', 'ABZ480', 'ABZ470', 'ABZ101', 'ABZ68', 'ABZ211', 'ABZ172', 'ABZ86', 'ABZ128', 'ABZ59', 'ABZ328', 'ABZ230', 'ABZ366', 'ABZ296', 'ABZ74', 'ABZ61', 'ABZ331e+152i', 'ABZ313', 'ABZ371', 'ABZ376', 'ABZ232', 'ABZ457', 'ABZ5', 'ABZ383', 'ABZ396', 'ABZ589', 'ABZ353', 'ABZ55', 'ABZ308', 'ABZ167', 'ABZ73', 'ABZ411', 'ABZ536', 'ABZ112', 'ABZ334', 'ABZ367', 'ABZ319', 'ABZ7', 'ABZ570', 'ABZ306', 'ABZ397', 'ABZ212', 'ABZ147', 'ABZ80', 'ABZ104', 'ABZ575', 'ABZ207', 'ABZ532', 'ABZ145', 'ABZ84', 'ABZ335', 'ABZ60', 'ABZ401', 'ABZ115', 'ABZ170', 'ABZ330', 'ABZ79', 'ABZ38', 'ABZ312', 'ABZ78', 'ABZ111', 'ABZ97', 'ABZ535', 'ABZ12', 'ABZ595', 'ABZ72', 'ABZ85', 'ABZ322', 'ABZ314', 'ABZ554', 'ABZ339', 'ABZ9', 'ABZ6', 'ABZ143', 'ABZ468', 'ABZ62', 'ABZ191', 'ABZ331', 'ABZ437', 'ABZ87', 'ABZ94', 'ABZ138', 'ABZ398', 'ABZ598a', 'ABZ440', 'ABZ465', 'ABZ152', 'ABZ134', 'ABZ393', 'ABZ298', 'ABZ50']
@@ -90,4 +90,3 @@ if __name__ == '__main__':
     signs = db['signs']
     sign_to_abz_dict = build_sign_to_abz_dict()
     value_to_abz_dict = generate_value_to_abz_dict(signs, sign_to_abz_dict)
-
